1c49cb1b8598d155baf406fff60c23bc1ae290a8
bcitozi <bcitozi@intelycare.com>
2021-10-30 15:26:59 +0200

    Layout In Correct Way (Advanced Design Needed)

--- OnlineGame/routes/web.php
+++ OnlineGame/routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [HomeController::class, 'index'])->name('home.index');
+Route::get('/', [HomeController::class, 'index'])->name('pages.home');
 
 Route::get("login",[AuthController::class, 'login'])->name('login') ;
 Route::get('register', [AuthController::class, 'register'])->name('register');
