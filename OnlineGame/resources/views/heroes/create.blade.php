33060463c491a88a3f74453cdf593a984b254dd6
xhoi-hub <xhuendhysa@gmail.com>
2021-11-07 16:51:32 +0100

    last commit

---
+++ OnlineGame/resources/views/heroes/create.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.panel')
+@section('content')
+    <!-- ! Main -->
+    <main class="main" id="skip-target">
+        <div class="container">
+            <h2 class="main-title">Heroes Table</h2>
+            <div class="media-content">
+                <div class="row">
+                    <div class="col-lg-3">
+                        <button class="btn-primary btn"type="submit">Create Hero's</button>
+                        <div class="media-sidebar">
+                            <ul class="quick-links">
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
